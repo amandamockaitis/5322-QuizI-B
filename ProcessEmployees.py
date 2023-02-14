1b968647454e14df28657a0e67b4e4b2da443ee5
amandamockaitis <amanda_mockaitis1@baylor.edu>
2023-02-14 14:49:55 -0600

    modified file to solve problem

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -1,4 +1,4 @@
-'''
+"""
 The Customer Service Represetatives (CSRs) in the marketing department with a security clearance of 'TS' were able
 to thwart an attack on the server and for that management has decided to reward them with a 10% increase in their salary. 
 To evaluate the impact on the budget, you have been asked to run a report on the employee file and display the results 
@@ -8,42 +8,46 @@ salary (as shown in the image).
 You will create a dictionary that has the full employee name as the key and only their NEW salary as the value.
 
 Iternate through the dictionary to print out their name and thier new salary (as shown in the image)
-'''
+"""
 
 import csv
 
-#open the file
+# open the file
+
+emp_file = open("employee_data.csv", "r")
+employee = csv.reader(emp_file)
 
 
+# create an empty dictionary
 
+emp_dict = {}
 
-#create an empty dictionary
+# use a loop to iterate through the csv file
 
+for row in employee:
+    Employee_Name = row[1] + " " + row[2]
+    salary = row[5]
+    clearance = row[9]
 
-#use a loop to iterate through the csv file
+    # check if the employee fits the search criteria
 
+    if clearance == "TS":
+        print(f"Employee_Name: {Employee_Name} Current Salary: {salary}")
+        emp_dict[Employee_Name] = float(salary) + float(salary) * 0.10
 
-    #check if the employee fits the search criteria
-
-
-    
 
 print()
-print('=========================================')
+print("=========================================")
 print()
 
-#iternate through the dictionary and print out the key and value as per image
+# iternate through the dictionary and print out the key and value as per image
 
 
-
-
-          
+for key, value in emp_dict.items():
+    print(f"Employee Name: {key} New Salary: {value}")
 
 print()
-print('=========================================')
+print("=========================================")
 print()
 
-#print out the total difference between the old salary and the new salary as per image.
-
-        
-    
+# print out the total difference between the old salary and the new salary as per image.
